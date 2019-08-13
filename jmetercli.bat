02a0f509c04da9938d9c463a280dde75e1d95218
AdanYao <adanyao407@gmail.com>
2019-08-13 17:26:43 +0800

    updateicash08131726

--- jmetercli.bat
+++ jmetercli.bat
@@ -2,8 +2,7 @@
 
 cd "\jmeter\apache-jmeter-5.1.1\apache-jmeter-5.1.1\bin"
 
-jmeter -n -t Step2會員登入壓力測試.jmx -l result.jtl -e -o C:\icash2\0731\loginreport
-
+jmeter -n -t C:\Final_IcashEndurance\腳本\Endurance\Step2會員登入壓力測試.jmx -l result.jtl -e -o C:\icash2\0812\loginreport
 
 
 ECHO finish

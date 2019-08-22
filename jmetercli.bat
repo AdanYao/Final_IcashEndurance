708ea11f077147cc698fd9741fa5e9640257e744
AdanYao <adanyao407@gmail.com>
2019-08-22 17:14:17 +0800

    update08221714

--- jmetercli.bat
+++ jmetercli.bat
@@ -1,10 +1,10 @@
 @ECHO OFF
 
-cd "\jmeter\apache-jmeter-5.1.1\apache-jmeter-5.1.1\bin"
+cd "\apache-jmeter-5.1.1\bin\"
 
-jmeter -n -t C:\Final_IcashEndurance\腳本\Endurance\Step2會員登入壓力測試.jmx -l result.jtl -e -o C:\icash2\0812\loginreport
-
+jmeter -n -t C:\Final_IcashEndurance\腳本\Endurance\Step2會員登入壓力測試.jmx -R 172.16.137.143:1099,172.16.137.142:1099 -l result.csv  -e -o C:\icash2\0822\loginreport 
 
 ECHO finish
+
 
 
